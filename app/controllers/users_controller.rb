00abbdb846ecf895d4d3b0d440e61c679747b9de
Hon Lam <lamh85@gmail.com>
2015-05-18 20:31:55 -0700

    Deleted devise views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  # CREATE
+
   def new
     @user = User.new
   end
@@ -16,6 +18,11 @@ class UsersController < ApplicationController
     end
   end
 
+  # READ
+
+
+  # UPDATE
+
   def update
     # render text: params
     if current_user.update(user_params)
@@ -30,6 +37,12 @@ class UsersController < ApplicationController
       render "/settings/"
     end
   end
+
+  # DESTROY
+
+  # OTHER
+
+  private
 
   def user_params
     params.require(:user).permit(
