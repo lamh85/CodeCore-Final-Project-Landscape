e9547a89746fe8205bb214bf33c8902cbaf39b6d
Hon Lam <lamh85@gmail.com>
2015-11-17 01:14:12 -0800

    Mkt Searches Angular: start building form fields as array.

--- app/assets/javascripts/market_searches_angular.js
+++ app/assets/javascripts/market_searches_angular.js
@@ -1,1 +1,20 @@
-console.log('testing');
+var marketSearches = angular.module("marketSearches",[]);
+
+mktSearchController = marketSearches.controller("mktSearchController", ['$scope', function($scope){
+
+    console.log('testing');
+
+    $scope.params = [];
+
+    var singleSearchParam = function() {
+        this.search_term = "";
+        this.property = ""
+    }
+
+    $scope.params.push(
+        new singleSearchParam()
+    );
+
+    console.log($scope.params);
+
+}]);
