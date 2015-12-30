060d456e8726381cee97f0de94fb56f1c7faa5d7
Hon Lam <lamh85@gmail.com>
2015-12-30 15:01:13 -0800

    Created Countries model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228233011) do
+ActiveRecord::Schema.define(version: 20151230230051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 20151228233011) do
   end
 
   add_index "competitions", ["organization_id"], name: "index_competitions_on_organization_id", using: :btree
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "filters", force: :cascade do |t|
     t.string   "group"
