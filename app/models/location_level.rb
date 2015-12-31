e7a676f58ba9b3f434ea05af088b48b21ecfd56e
Hon Lam <lamh85@gmail.com>
2015-12-31 11:57:12 -0800

    LocationLevel: debugging parent_level method.

--- app/models/location_level.rb
+++ app/models/location_level.rb
@@ -4,5 +4,16 @@ class LocationLevel < ActiveRecord::Base
 
     # Relationships
     has_many :locations
-    belongs_to :parent
+
+    def parent_level
+        level_value = self.level - 1
+        while level_value >= 1
+            results = LocationLevel.where('level = #{level_value} AND thread = #{self.thread}')
+            if results.length > 0
+                return results
+            else
+                level_value -= 1
+            end
+        end
+    end
 end
