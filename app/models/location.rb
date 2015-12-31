cbc6c7e5d6d6fbd4eddcbc3587839d1aa528e22a
Hon Lam <lamh85@gmail.com>
2015-12-30 18:08:44 -0800

    Added validation rule to Location.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,6 @@
 class Location < ActiveRecord::Base
     # Validations
+    validates :name, presence: {message: "This location requires a name."}
 
     # Relationships
     belongs_to :location_level
