da5aade86d067a2cab9061c2a04f152b9064b2b8
Hon Lam <lamh85@gmail.com>
2016-01-06 21:38:34 -0800

    LocationLevel Angular: started coding the sorting by thread.

--- app/assets/javascripts/location_levels_angular.js
+++ app/assets/javascripts/location_levels_angular.js
@@ -2,11 +2,28 @@ var locationLevelApp = angular.module("locationLevelApp",[]);
 
 locationLevelController = locationLevelApp.controller("locationLevelController", ['$scope', '$http', function($scope, $http){
 
-    $scope.levels = [];
-    $http.get('../location_levels/get_all').then(function(data){
-        console.log('data retrieved!');
-        window.debugVar = data;
+    var sortByThread = function(a,b) {
+        if (a.thread == b.thread) {
+            return 0;
+        } else if (a.thread > b.thread) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    var levelsRaw = [];
+    $scope.levelsSegmented = [];
+
+    var divideLevels = function() {
+        levelsRaw = levelsRaw.sort(sortByThread);
+    }
+
+    $http.get('../location_levels/get_all').then(function(response){
+        levelsRaw = response.data;
+        console.log('GET successful');
     })
 
-    console.log('I am from location_level_angular');
+    // Create an array for each thread
+
 }]);
