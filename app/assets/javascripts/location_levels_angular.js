431cf077b23b6a16041db1aaba61883239e0eb59
Hon Lam <lamh85@gmail.com>
2016-01-06 21:07:16 -0800

    LocationLevel Angular: started editing GET.

--- app/assets/javascripts/location_levels_angular.js
+++ app/assets/javascripts/location_levels_angular.js
@@ -1,6 +1,12 @@
 var locationLevelApp = angular.module("locationLevelApp",[]);
 
 locationLevelController = locationLevelApp.controller("locationLevelController", ['$scope', '$http', function($scope, $http){
-    
+
+    $scope.levels = [];
+    $http.get('../location_levels/get_all').then(function(data){
+        console.log('data retrieved!');
+        window.debugVar = data;
+    })
+
     console.log('I am from location_level_angular');
 }]);
