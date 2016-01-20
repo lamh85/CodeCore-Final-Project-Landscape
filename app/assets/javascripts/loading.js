1718a919246fb59b1b2d66e026f885e90d14e6c3
Hon Lam <lamh85@gmail.com>
2016-01-19 22:08:59 -0800

    Successfully added LOADING function to XMLHttpRequest prototype.

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -2,17 +2,6 @@
 var intervalVariable;
 var deactivateLoading;
 
-  // save the real `send`
-  var realSend = XMLHttpRequest.prototype.send;
-
-  // replace `send` with a wrapper
-  XMLHttpRequest.prototype.send = function() {
-      console.log('The \"send\" function is running');
-
-      // run the real `send`
-      realSend.apply(this, arguments);
-  }
-
 $(document).ready(function() {
 
   window.deactivateLoading = function() {
@@ -30,16 +19,26 @@ $(document).ready(function() {
     intervalVariable = setInterval(function(){
       $('#loading .glyphicon-refresh').css('transform','rotate('+degrees+'deg)');
       degrees += 1;
-      console.log("Degrees:" +degrees);
     }, 3); // setInterval
     $('#loading-shell').show();
   }
 
-  $('a').click(activateLoading);
-
   // On load, stop loading animation
   if (typeof(intervalVariable) != "undefined") {
     clearInterval(intervalVariable);
+  }
+
+  $('a').click(activateLoading);
+
+  // save the real `send`
+  var realSend = XMLHttpRequest.prototype.send;
+
+  // replace `send` with a wrapper
+  XMLHttpRequest.prototype.send = function() {
+      window.activateLoading();
+
+      // run the real `send`
+      realSend.apply(this, arguments);
   }
 
 }) // Close the ready method
